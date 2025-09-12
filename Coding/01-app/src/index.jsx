30951a85f172117be088c4d8773b9d028e8db03d
ashwaniraii <ashwanirai975@gmail.com>
2025-09-12 21:53:26 +0530

    changes

--- Coding/01-app/src/index.jsx
+++ Coding/01-app/src/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Ashwani Rai</h1>);
+root.render(<h1 className="text-red-900">Ashwani Rai</h1>);
